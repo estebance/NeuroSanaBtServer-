84b327300bd1da61131824bf8e892b931beb44bc
estebance <restebance@gmail.com>
2013-10-24 23:41:45 -0500

    Añadir cambios input y output streams

--- src/com/bt/service/ManageConnectionThread.java
+++ src/com/bt/service/ManageConnectionThread.java
@@ -16,8 +16,8 @@ import com.bt.loadfile.ChargeFile;
 public class ManageConnectionThread implements Runnable 
 {
 	StreamConnection My_Connection;		
-    private  OutputStream MyOutStream;
-    private  InputStream MyInputStream;
+   // private  OutputStream MyOutStream;
+   // private  InputStream MyInputStream;
     private static final int COMANDO_SALIR = 1;
 	private static final int COMANDO_ENVIAR = 2;
 	private static final int COMANDO_CANCELAR=3;
@@ -31,17 +31,17 @@ public class ManageConnectionThread implements Runnable
    public ManageConnectionThread( StreamConnection connection )
 	{
 	  My_Connection = connection;	 
-      InputStream tmpInput = null;
-      OutputStream tmpOutput = null;
-     try 
-      {
-          tmpInput = My_Connection.openInputStream();
-          tmpOutput = My_Connection.openOutputStream();
-      } 
-      catch (IOException e) {}
+    //  InputStream tmpInput = null;
+    //  OutputStream tmpOutput = null;
+    // try 
+    //  {
+    //      tmpInput = My_Connection.openInputStream();
+    //      tmpOutput = My_Connection.openOutputStream();
+    //  } 
+    //  catch (IOException e) {}
 
-      MyInputStream = tmpInput;
-      MyOutStream = tmpOutput;
+     // MyInputStream = tmpInput;
+     // MyOutStream = tmpOutput;
   
   }
 	  
@@ -57,12 +57,15 @@ public class ManageConnectionThread implements Runnable
 	try 
 	{
 		System.out.println("esperando una solicitud por parte del cliente");
-		DataInputStream entrada = new DataInputStream(MyInputStream);			
+		//DataInputStream entrada = new DataInputStre,am(MyInputStream);	
+		InputStream MyInputStream = My_Connection.openInputStream();
 		while(true)
 		{			
-		System.out.println(entrada.readUTF());			
-		int comando = Integer.parseInt(entrada.readUTF());
-	    if(comando == COMANDO_SALIR)
+		System.out.println(MyInputStream.read());			
+		//int comando = Integer.parseInt(MyInputStream.read());
+		int comando = MyInputStream.read();
+	    
+		if(comando == COMANDO_SALIR)
 		{
 		System.out.println("salimos del servidor");
 		closeall();
@@ -74,6 +77,8 @@ public class ManageConnectionThread implements Runnable
 	    }
 	    
 		}
+		
+		MyInputStream.close();
 		
 	}
 	catch(Exception e)
@@ -90,7 +95,10 @@ public class ManageConnectionThread implements Runnable
 		// TODO Auto-generated method stub
 		System.out.println("el proceso es " + command);
 		
-		/*
+		try
+		{
+		OutputStream MyOutputStream = My_Connection.openOutputStream();	
+		
 		if(command == COMANDO_ENVIAR)
 		{	
 	    ChargeFile sendata = new ChargeFile();
@@ -98,32 +106,18 @@ public class ManageConnectionThread implements Runnable
 	    sendata.SetRouteData("una ruta");
 	    byte[] informacion = sendata.DataFile();
 	    String nombrearchivo = sendata.GetNameFile();
-        try 
-        {	
-         DataOutputStream flujo= new DataOutputStream( MyOutStream );
-         flujo.write(informacion);
-         // cerramos para evitar problemas
-         flujo.close();
-        } 
-        
-        catch (IOException e) { }			
+
+        MyOutputStream.write(informacion);
+        MyOutputStream.close();
+	
 		}
-		*/
+
 		if(command == COMANDO_INICIAR)
-		{
-		
+		{		
 		/* ordenes para iniciar la captura de archivos cuando finalmente termine 
-		   respondera con un comando terminado*/	
-	     try 
-		      {
-		      DataOutputStream flujo= new DataOutputStream(MyOutStream);
-		      flujo.writeUTF(Integer.toString(COMANDO_TERMINADO));
-		      flujo.close();
-		      } 
-		      catch (IOException e) 
-		      {
-		 	  System.out.println("error al responder la solicitud:"+e);    
-		      }	
+		   respondera con un comando terminado*/		      
+		      MyOutputStream.write(COMANDO_TERMINADO);
+		      MyOutputStream.close(); 
 		}
 		
 		if(command == COMANDO_CANCELAR)
@@ -131,19 +125,20 @@ public class ManageConnectionThread implements Runnable
 			/* ordenes para cancelar la toma de archivos se supone  este deberia buscar 
 			 * el archivo hasta el momento generado y borrarlo	
 			 */
-		 try 
-	     {
-			      DataOutputStream flujo= new DataOutputStream(MyOutStream);
-			      flujo.writeUTF(Integer.toString(COMANDO_CANCELADO));
-			      flujo.close();
-	     } 
-	     catch (IOException e) 
-	     {
-		  System.out.println("error al responder la solicitud:"+e);    
-	     }			
+		      MyOutputStream.write(COMANDO_CANCELADO);
+		      MyOutputStream.close(); 
 			
-		}
+	     }
 		
+		 MyOutputStream.close(); 
+		
+		 }
+		 catch(Exception e)
+		 {
+		 System.out.println("error en process command" +e); 	
+		 }
+		
+
 		
 	}
 	
@@ -153,9 +148,6 @@ public class ManageConnectionThread implements Runnable
 		
 	try
 	{
-		MyOutStream.close();
-		MyInputStream.close();
-		// puede que este no funcione 
 		My_Connection.close();
 	} 
 	
